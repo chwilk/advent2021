1149c0faff64fd00b139f96b0e69741623068db9
chwilk <chwilk@gmail.com>
2021-12-13 17:30:49 -0600

    Day 7 part 1

---
+++ src/bin/advent7.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs;
+use advent2021::find_filename;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = find_filename(&args);
+    let contents = fs::read_to_string(filename).expect("File error");
+    let mut lines = contents.lines();
+    let crabs: Vec<i32> = lines.next().unwrap().split(',').filter_map(|x| x.parse::<i32>().ok()).collect();
+
+    let min = crabs.iter().min().unwrap();
+    let max = crabs.iter().max().unwrap();
+
+    let mut minima = 1451499;
+    let mut ideal = 0;
+
+    println!("Min/Max {}/{}", min, max);
+    // Scan all the values!
+    for i in (*min as usize)..=(*max as usize) {
+        let costi = cost(&crabs, i.try_into().unwrap());
+        if costi < minima {
+            minima = costi;
+            ideal = i;
+        }
+        println!("{}: {}", i, costi);
+    }
+
+    println!("Lowest cost at {} with {}", ideal, minima);
+}
+
+fn cost(crabs: &Vec<i32>, pos: i32) -> i32 {
+    let mut sum: i32 = 0;
+    for c in crabs {
+        sum += (c - pos).abs();
+    }
+    sum
+}
