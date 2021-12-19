3a5aed74cf9bfdee2cb11360adbfc4cf426a2059
chwilk <chwilk@gmail.com>
2021-12-18 22:00:28 -0600

    day10 part1

---
+++ src/bin/advent10.rs
@@ -0,0 +1,61 @@
+use std::env;
+use std::fs;
+use advent2021::find_filename;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = find_filename(&args);
+    let contents = fs::read_to_string(filename).expect("File error");
+
+    let mut total_score = 0;
+
+
+    'lines: for l in contents.lines() {
+        let mut stack = Vec::new();
+        let tokens = l.chars();
+        for t in tokens {
+            if is_open(t) {
+                stack.push(t);
+            } else {
+                let o = stack.pop().unwrap();
+                if !closes(o, t) {
+                    total_score += score(t);
+                    println!("got {} when closing {}", t, o);
+                    continue 'lines;
+                }
+            }
+        }
+        println!();
+    }
+    println!("Total score: {}", total_score);
+}
+
+fn score(c: char) -> u32 {
+    match c {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _   => 0,
+    }
+}
+
+fn is_open(c: char) -> bool {
+    match c {
+        '(' => true,
+        '[' => true,
+        '{' => true,
+        '<' => true,
+        _   => false,
+    }
+}
+
+fn closes(o: char, c: char) -> bool {
+    match o {
+        '(' => c == ')',
+        '[' => c == ']',
+        '{' => c == '}',
+        '<' => c == '>',
+        _   => false,
+    }
+}
